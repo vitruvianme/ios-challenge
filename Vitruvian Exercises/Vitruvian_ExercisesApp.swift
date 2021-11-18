ca5cdb2e4a84fad705593f6f5b320cdc00245953
Izaak Sultan <izaaksultan@gmail.com>
2021-11-18 12:38:38 +0800

    Initial Commit

---
+++ Vitruvian Exercises/Vitruvian_ExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vitruvian_ExercisesApp.swift
+//  Vitruvian Exercises
+//
+//  Created by Izaak Sultan on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Vitruvian_ExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
